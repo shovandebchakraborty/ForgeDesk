a0207b8829a8158e5f55ffefd14ca47112052513
Developer Shovandeb <shovandeb2004@gmail.com>
2026-06-30 12:43:38 +0530

    Update views.py

--- Backend/manage/views.py
+++ Backend/manage/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import *
+from django.views.decorators.csrf import csrf_exempt
+import json
 # Create your views here.
+@csrf_exempt
+def ProjectCreateAPI(request):
+    data = json.loads(request.body)
+
+    if request.method == 'POST':
+        projectIcon = data.get('icon')
+        projectName = data.get('projectName')
+        projectDescription = data.get('Description')
+
+        ProjectStructure.objects.create(
+            Icon = projectIcon,
+            Name = projectName,
+            Description = projectDescription
+        )
+
+        DataResponse = []
+        for get in ProjectStructure.objects.all():
+            DataResponse.append(get.Name)
+
+        return JsonResponse({
+            "Project-status" : "Done",
+            "Project-Name" : DataResponse,
+            "Message" : "Uploaded"
+        })
