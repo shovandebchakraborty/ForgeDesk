d7aaeaa2cfe188d7fe524b4959c2516333bcd481
Developer Shovandeb <shovandeb2004@gmail.com>
2026-06-30 12:43:12 +0530

    Update urls.py

--- Backend/manage/urls.py
+++ Backend/manage/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-
+from .views import *
 urlpatterns = [
-    path()
+    path('projectcreateapi/',ProjectCreateAPI,name="project_create_api")
 ]
